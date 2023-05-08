3fd2334ead4b52269451b1f769af6f75bc35073f
Syat <titmouse99@mail.ru>
2023-05-08 14:19:09 +0300

    1 задача

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        task1();
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName  = " Ivan ";
+        String middleName = " Ivanovich ";
+        String lastName = " Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
     }
 }
